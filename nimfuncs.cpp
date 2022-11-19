4a08996684ce22f26b5b516d8274ff0b8c867819
mcheung008 <62771803+mcheung008@users.noreply.github.com>
2022-11-19 12:38:40 -0800

    Initial commit

---
+++ nimfuncs.cpp
@@ -0,0 +1,16 @@
+//
+// Created by mcheu on 11/19/2022.
+//
+#include <iostream>
+#include "nimfuncs.h"
+
+void showInstructions()
+{
+std::cout << "Display instructions" << std::endl;
+}
+void showBoard(int num_sticks);
+int getMove(int num_sticks);
+bool playAgain();
+
+
+
---
+++ nimfuncs.h
@@ -0,0 +1,16 @@
+//
+// Created by mcheu on 11/19/2022.
+//
+#ifndef LAB8_NIM2_0_NIMFUNCS_H
+#define LAB8_NIM2_0_NIMFUNCS_H
+
+
+// function prototypes
+void showInstructions();
+void showBoard(int num_sticks);
+int getMove(int num_sticks);
+bool playAgain();
+
+
+
+#endif //LAB8_NIM2_0_NIMFUNCS_H
