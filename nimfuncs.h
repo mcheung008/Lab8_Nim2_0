27bf98a4735ae36f70813aff43c9be82a4d81a8e
mcheung008 <62771803+mcheung008@users.noreply.github.com>
2022-11-20 22:37:34 -0800

    Final commit before submission.

--- nimfuncs.h
+++ nimfuncs.h
@@ -4,13 +4,11 @@
 #ifndef LAB8_NIM2_0_NIMFUNCS_H
 #define LAB8_NIM2_0_NIMFUNCS_H
 
-
 // Function prototypes.
 void showInstructions();
 void showBoard(int num_sticks);
 int getMove(int num_sticks);
 bool playAgain();
-
 
 
 #endif //LAB8_NIM2_0_NIMFUNCS_H
